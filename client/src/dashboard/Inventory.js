a108a54eeb936649447b66b2bff8ef62d7b06fe4
aishabraud92 <aishabraud@gmail.com>
2018-02-01 15:46:22 -0800

    update inventory

--- client/src/dashboard/Inventory.js
+++ client/src/dashboard/Inventory.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import AddItem from './AddItem';
+
 
 class Inventory extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Inventory</h1>
+				<AddItem />
 				<div>
-					<h2>Items you have to lend out.</h2>
-					<div className="items">
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
-						<h3>Item</h3>
+					<h1>Inventory</h1>
+					<div>
+						<h2>Items you have to lend out.</h2>
+						<div className="items">
+						<h3>Add Items To you inventory</h3>
+
+						</div>
 					</div>
 				</div>
 			</div>
