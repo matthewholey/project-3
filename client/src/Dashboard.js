5bf8bb3088bd220b856a04c652a74dea617d0fb1
aishabraud92 <aishabraud@gmail.com>
2018-02-02 16:04:57 -0800

    update

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-=======
+
 
 import Dashbar from './dashboard/Dashbar.js';
 
 import Borrowing from './dashboard/Borrowing.js';
->>>>>>> 357e8ce203006ecfd045a8399e3da8fc9b326c91
+
 import Inventory from './dashboard/Inventory.js';
 import Lending from './dashboard/Lending.js';
 
@@ -18,7 +17,7 @@ class Dashboard extends Component {
     if (this.props.user && this.props.user.name) {
       page = (
         <div>
-<<<<<<< HEAD
+
           <header className="App-header">
             <Link to="/lending"> Lending </Link>
             <Link to="/inventory"> Inventory </Link>
@@ -26,7 +25,7 @@ class Dashboard extends Component {
           </header>
           <Inventory />
           <Needed />
-=======
+
           <Dashbar />
           <div>
             <Link to="/needed">Needed</Link>
@@ -38,7 +37,7 @@ class Dashboard extends Component {
               </div>
             </div>
           </div>
->>>>>>> 357e8ce203006ecfd045a8399e3da8fc9b326c91
+
         </div>
       );
     }
