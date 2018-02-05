4f215981e7e55241881f91201ee73139b32aaa31
aishabraud92 <aishabraud@gmail.com>
2018-02-04 16:34:15 -0800

    Update Inventory.js

--- client/src/dashboard/Inventory.js
+++ client/src/dashboard/Inventory.js
@@ -5,50 +5,6 @@ import AddItem from './AddItem';
 class Inventory extends Component {
 	render() {
 		return (
-		var NewComponent = React.createClass({
-  displayName: "NewComponent",
-
-  render: function () {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "h2",
-        { className: "text-center" },
-        "Items"
-      ),
-      React.createElement(
-        "form",
-        { method: "POST", action: "/items" },
-        React.createElement(
-          "span",
-          null,
-          React.createElement("input", { name: "title", type: "text", className: "form-control", required: true }),
-          React.createElement("input", { className: "btn btn-primary", type: "submit", defaultValue: "Add Item" })
-        )
-      ),
-    	React.item.forEach(this.props.item, (item, i) => {
-  		console.log(items + ' at index: ' + i);
-})
-      
-      
-      	React.createElement("div", null),
-      	React.createElement(
-        "h5",
-        null,
-        React.createElement(
-          "a",
-          { href: "/item/'(item.id)},
-          (item.title)
-        )
-      ),
-      React.createElement(
-        "a",
-        { href: "/boards/<%= item.id %>", className: "btn btn-danger delete-link" },
-        "Delete Item"
-      )
-    );
-  }
-});
+		
 
 export default Inventory;
